0d703c7b608c15ec695faf461adb3a8976c157ff
Vijaysankar-R <105067556+Vijaysankar-R@users.noreply.github.com>
2024-09-08 22:06:17 +0530

    Update main.c

--- Compilation_Process/main.c
+++ Compilation_Process/main.c
@@ -1,1 +1,12 @@
-
+#include<stdio.h>
+int main()
+{
+  int myInt;
+  float myFloat;
+  printf("Enter any Integer:");
+  scanf("%d",&myInt);
+  printf("Enter any Float:");
+  scanf("%f",&myFloat);
+  printf("The size of %d is %lld\n",myInt,sizeof(myInt));
+  printf("The size of %f is %lld\n",myFloat,sizeof(myFloat));
+}
